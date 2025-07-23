eaccf0d4dec37ed0d45f8dbb2502432bae3f71ee
Your Name <your.email@example.com>
2025-07-23 19:33:44 +0530

    Initial commit

---
+++ java-ci-demo/src/main/java/com/example/java_ci_demo/JavaCiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.java_ci_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCiDemoApplication.class, args);
+	}
+
+}
---
+++ java-ci-demo/src/test/java/com/example/java_ci_demo/JavaCiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_ci_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
